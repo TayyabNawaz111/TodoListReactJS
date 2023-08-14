401666be10c86a9ec0fa25e5ffbffa6ed74cfa2f
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2023-08-14 13:53:00 +0500

    Imported Todos to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Todos from "./Components/Todos";
 function App() {
+  let todos = [
+    {
+      sno: 1,
+      title: "Learn React",
+      desc: "This is a react tutorial",
+    },
+    {
+      sno: 2,
+      title: "Learn Nodejs",
+      desc: "This is node js tutorial",
+    },
+  ];
   return (
     <>
-      <Header title="Todos List" />
+      <Header title="Todos List" key = {todos.sno}  />
+      <Todos todos={todos} />
     </>
   );
 }
