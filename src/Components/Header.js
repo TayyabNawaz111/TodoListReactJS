a858fd8b9df588a2f0486fe9c237e049715566ce
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2023-08-14 12:30:09 +0500

    Used props instead of plain text

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand">Todos List</a>
+        <a className="navbar-brand">{props.title}</a>
       </div>
     </nav>
   );
