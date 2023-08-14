fbc19d1da2991004f8fe802d1f1bbd5c40868657
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2023-08-14 12:19:50 +0500

    Added Header

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand">Todos List</a>
+      </div>
+    </nav>
+  );
+}
