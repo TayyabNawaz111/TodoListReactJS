eac971623d664038ca7c64dd94e9eb6a5ae9cf84
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2023-08-14 15:29:48 +0500

    Added NewTodo Component

---
+++ src/Components/NewTodo.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export default function NewTodo(props) {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const submit = (parameter) => {
+    parameter.preventDefault();
+    if (!title || !desc) {
+      alert("Error! Title/Desc is blank");
+    } else {
+      props.addTodo(title, desc);
+    }
+  };
+
+  return (
+    <div>
+      <h3>Add a new todo</h3>
+      <form onSubmit={submit}>
+        <div>
+          <label>Title</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Add Title Here"
+            value={title}
+            onChange={(change) => {
+              setTitle(change.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label>Description</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Add Description here"
+            value={desc}
+            onChange={(change) => {
+              setDesc(change.target.value);
+            }}
+          />
+        </div>
+        <button className="btn btn-success">Submit</button>
+      </form>
+    </div>
+  );
+}
