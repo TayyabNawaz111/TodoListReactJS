45f120f50cebb1c001b5146d2c45b787c466d33d
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2023-08-14 13:51:47 +0500

    Added AddTodo.js

---
+++ src/Components/AddTodo.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function AddTodo(props) {
+  return (
+    <div>
+      <h4>{props.todo.title}</h4>
+      <p>{props.todo.desc}</p>
+      <button className="btn btn-danger btn-sm">Delete</button>
+    </div>
+  );
+}
