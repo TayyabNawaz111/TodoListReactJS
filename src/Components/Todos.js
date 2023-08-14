da2262e9cfb857524ca1633270890b0e86bd2c4e
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2023-08-14 15:28:55 +0500

    Added validation in Todos.js

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -4,9 +4,13 @@ import React from "react";
 export default function Todos(props) {
   return (
     <div>
-      {props.todos.map((todo) => {
-        return (<AddTodo todo={todo} key={todo.sno} />)
-      })}
+      {props.todos.length === 0
+        ? "No todos"
+        : props.todos.map((todo) => {
+            return (
+              <AddTodo todo={todo} key={todo.sno} onDelete={props.onDelete} />
+            );
+          })}
     </div>
-  )
+  );
 }
